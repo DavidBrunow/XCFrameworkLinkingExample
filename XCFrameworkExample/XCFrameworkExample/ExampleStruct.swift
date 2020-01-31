15dd3873c3f684cd436659d07f769ef5f1d793d4
David Brunow <david@shep.dog>
2020-01-31 16:25:13 -0600

    Import sample XCFramework in Swift file (ExampleApp build fails)

---
+++ XCFrameworkExample/XCFrameworkExample/ExampleStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ExampleStruct.swift
+//  XCFrameworkExample
+//
+//  Created by David Brunow on 1/31/20.
+//  Copyright © 2020 XCFrameworkExample. All rights reserved.
+//
+
+import Foundation
+import CoconutLib
+
+struct ExampleStruct {
+    
+}
