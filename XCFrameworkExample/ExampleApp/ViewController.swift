ef6a4a809b4f28134de004e203b3e05ef6b09298
David Brunow <david@shep.dog>
2020-01-31 16:20:18 -0600

    Add example app

---
+++ XCFrameworkExample/ExampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by David Brunow on 1/31/20.
+//  Copyright © 2020 XCFrameworkExample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
