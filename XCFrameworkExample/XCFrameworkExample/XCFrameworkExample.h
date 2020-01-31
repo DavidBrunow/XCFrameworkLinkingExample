995687ad3882527c8c49bec67b817ca2c6d7ba98
David Brunow <>
2020-01-31 16:18:11 -0600

    Create new project in Xcode

---
+++ XCFrameworkExample/XCFrameworkExample/XCFrameworkExample.h
@@ -0,0 +1,19 @@
+//
+//  XCFrameworkExample.h
+//  XCFrameworkExample
+//
+//  Created by David Brunow on 1/31/20.
+//  Copyright © 2020 XCFrameworkExample. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XCFrameworkExample.
+FOUNDATION_EXPORT double XCFrameworkExampleVersionNumber;
+
+//! Project version string for XCFrameworkExample.
+FOUNDATION_EXPORT const unsigned char XCFrameworkExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCFrameworkExample/PublicHeader.h>
+
+
